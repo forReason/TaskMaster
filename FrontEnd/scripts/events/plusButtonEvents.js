ed80fc5c2e3e8548286b0b3da49b5be44a28bab8
julianbechtold <julian.bechtold@outlook.com>
2024-12-16 22:14:25 +0100

    style improvements

--- FrontEnd/scripts/events/plusButtonEvents.js
+++ FrontEnd/scripts/events/plusButtonEvents.js
@@ -9,9 +9,7 @@ export function setupPlusButton() {
         emptyTaskCard.style.top = '50%';
         emptyTaskCard.style.left = '50%';
         emptyTaskCard.style.transform = 'translate(-50%, -50%)';
-        emptyTaskCard.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
         emptyTaskCard.style.padding = '1rem';
-        emptyTaskCard.style.backgroundColor = 'white';
         emptyTaskCard.style.width = '300px';
         emptyTaskCard.style.zIndex = '10000';
 
